e5c3d2147940fd05503db5b3f77444c3fce7f7c9
habibium <habib.prodev@gmail.com>
2024-10-21 04:38:29 +0600

    chore: format project

--- public/index.js
+++ public/index.js
@@ -1,20 +1,19 @@
-
 const translateHandler = async () => {
   const textArea = document.getElementById("text-input");
   const localeArea = document.getElementById("locale-select");
   const errorArea = document.getElementById("error-msg");
   const translatedArea = document.getElementById("translated-sentence");
-  
+
   errorArea.innerText = "";
   translatedArea.innerText = "";
 
   const data = await fetch("/api/translate", {
     method: "POST",
     headers: {
-      "Accept": "application/json",
-      "Content-type": "application/json"
+      Accept: "application/json",
+      "Content-type": "application/json",
     },
-    body: JSON.stringify({"text": textArea.value, "locale": localeArea.value})
+    body: JSON.stringify({ text: textArea.value, locale: localeArea.value }),
   });
 
   const parsed = await data.json();
@@ -27,4 +26,6 @@ const translateHandler = async () => {
   return;
 };
 
-document.getElementById("translate-btn").addEventListener("click", translateHandler)
+document
+  .getElementById("translate-btn")
+  .addEventListener("click", translateHandler);
