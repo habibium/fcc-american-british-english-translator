05315f692f4ace4c4f057573b008c75809b98fa9
habibium <habib.prodev@gmail.com>
2024-10-21 14:04:40 +0600

    refactor: add translation methods to Translator class

--- components/translator.js
+++ components/translator.js
@@ -3,6 +3,26 @@ const americanToBritishSpelling = require("./american-to-british-spelling.js");
 const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
 
-class Translator {}
+class Translator {
+  toBritish(text) {
+    // TODO
+    return text;
+  }
+
+  toAmerican(text) {
+    // TODO
+    return text;
+  }
+
+  toBritishHighlighted(text) {
+    // TODO
+    return text;
+  }
+
+  toAmericanHighlighted(text) {
+    // TODO
+    return text;
+  }
+}
 
 module.exports = Translator;
