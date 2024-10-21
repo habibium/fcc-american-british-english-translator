48f8c4d985fa21bd0614a353ac2dfb58b7d65506
habibium <habib.prodev@gmail.com>
2024-10-21 15:12:13 +0600

    feat: add translation endpoint to API

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,22 @@ const Translator = require("../components/translator.js");
 module.exports = function (app) {
   const translator = new Translator();
 
-  app.route("/api/translate").post((req, res) => {});
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (!text || !locale)
+      return res.json({
+        error: "Required field(s) missing",
+      });
+
+    if (!Translator.LOCALES.includes(locale))
+      return res.json({
+        error: "Invalid value for locale field",
+      });
+
+    return res.json({
+      text: req.body.text,
+      translation: translator.translate(req.body.text, req.body.locale),
+    });
+  });
 };
